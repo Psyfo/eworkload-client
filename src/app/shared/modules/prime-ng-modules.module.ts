d57030298775cf2087eb245ea36c191dbf492754
Psyfo <21422869@dut4life.ac.za>
2020-02-17 14:39:09 +0200

    Added more primeng modules

--- src/app/shared/modules/prime-ng-modules.module.ts
+++ src/app/shared/modules/prime-ng-modules.module.ts
@@ -1,5 +1,6 @@
 import { ChartsModule } from 'ng2-charts';
 import { AccordionModule } from 'primeng/accordion';
+import { ConfirmationService } from 'primeng/api';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { BlockUIModule } from 'primeng/blockui';
 import { ButtonModule } from 'primeng/button';
@@ -9,8 +10,10 @@ import { ChartModule } from 'primeng/chart';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ChipsModule } from 'primeng/chips';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ContextMenuModule } from 'primeng/contextmenu';
 import { DropdownModule } from 'primeng/dropdown';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { FileUploadModule } from 'primeng/fileupload';
 import { InplaceModule } from 'primeng/inplace';
 import { InputTextareaModule } from 'primeng/inputtextarea';
@@ -19,9 +22,11 @@ import { ListboxModule } from 'primeng/listbox';
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
 import { MultiSelectModule } from 'primeng/multiselect';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { PanelModule } from 'primeng/panel';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { PasswordModule } from 'primeng/password';
+import { PickListModule } from 'primeng/picklist';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
@@ -32,97 +37,108 @@ import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { PageHeaderModule } from './page-header';
 import { SharedPipesModule } from '../pipes/shared-pipes.module';
+import { PageHeaderModule } from './page-header';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        AccordionModule,
-        AutoCompleteModule,
-        BlockUIModule,
-        ButtonModule,
-        CalendarModule,
-        CardModule,
-        ChartModule,
-        ChartsModule,
-        CheckboxModule,
-        ChipsModule,
-        CommonModule,
-        ContextMenuModule,
-        DropdownModule,
-        FileUploadModule,
-        FormsModule,
-        InplaceModule,
-        InputTextareaModule,
-        InputTextModule,
-        KeyFilterModule,
-        ListboxModule,
-        MessageModule,
-        MessagesModule,
-        MultiSelectModule,
-        PageHeaderModule,
-        PanelModule,
-        PanelMenuModule,
-        PasswordModule,
-        ProgressBarModule,
-        ProgressSpinnerModule,
-        ReactiveFormsModule,
-        ScrollPanelModule,
-        SharedPipesModule,
-        SidebarModule,
-        SliderModule,
-        SpinnerModule,
-        TableModule,
-        TabViewModule,
-        ToastModule,
-        ToolbarModule
-    ],
-    exports: [
-        AccordionModule,
-        AutoCompleteModule,
-        BlockUIModule,
-        ButtonModule,
-        CalendarModule,
-        CardModule,
-        ChartModule,
-        ChartsModule,
-        CheckboxModule,
-        ChipsModule,
-        CommonModule,
-        ContextMenuModule,
-        DropdownModule,
-        FileUploadModule,
-        FormsModule,
-        InplaceModule,
-        InputTextareaModule,
-        InputTextModule,
-        KeyFilterModule,
-        ListboxModule,
-        MessageModule,
-        MessagesModule,
-        MultiSelectModule,
-        PageHeaderModule,
-        PanelModule,
-        PanelMenuModule,
-        ProgressBarModule,
-        ProgressSpinnerModule,
-        PasswordModule,
-        ReactiveFormsModule,
-        ScrollPanelModule,
-        SharedPipesModule,
-        SidebarModule,
-        SliderModule,
-        SpinnerModule,
-        TableModule,
-        TabViewModule,
-        ToastModule,
-        ToolbarModule
-    ]
+  declarations: [],
+  imports: [
+    AccordionModule,
+    AutoCompleteModule,
+    BlockUIModule,
+    ButtonModule,
+    CalendarModule,
+    CardModule,
+    ChartModule,
+    ChartsModule,
+    CheckboxModule,
+    ChipsModule,
+    CommonModule,
+    ConfirmDialogModule,
+    ContextMenuModule,
+    DynamicDialogModule,
+    DropdownModule,
+    FileUploadModule,
+    FormsModule,
+    InplaceModule,
+    InputTextareaModule,
+    InputTextModule,
+    KeyFilterModule,
+    ListboxModule,
+    MessageModule,
+    MessagesModule,
+    MultiSelectModule,
+    OverlayPanelModule,
+    PageHeaderModule,
+    PanelModule,
+    PanelMenuModule,
+    PasswordModule,
+    PickListModule,
+    ProgressBarModule,
+    ProgressSpinnerModule,
+    ReactiveFormsModule,
+    ScrollPanelModule,
+    SharedPipesModule,
+    SidebarModule,
+    SliderModule,
+    SpinnerModule,
+    TableModule,
+    TabViewModule,
+    ToastModule,
+    ToolbarModule,
+    TooltipModule
+  ],
+  exports: [
+    AccordionModule,
+    AutoCompleteModule,
+    BlockUIModule,
+    ButtonModule,
+    CalendarModule,
+    CardModule,
+    ChartModule,
+    ChartsModule,
+    CheckboxModule,
+    ChipsModule,
+    CommonModule,
+    ConfirmDialogModule,
+    ContextMenuModule,
+    DropdownModule,
+    DynamicDialogModule,
+    FileUploadModule,
+    FormsModule,
+    InplaceModule,
+    InputTextareaModule,
+    InputTextModule,
+    KeyFilterModule,
+    ListboxModule,
+    MessageModule,
+    MessagesModule,
+    MultiSelectModule,
+    OverlayPanelModule,
+    PageHeaderModule,
+    PanelModule,
+    PanelMenuModule,
+    PickListModule,
+    ProgressBarModule,
+    ProgressSpinnerModule,
+    PasswordModule,
+    ReactiveFormsModule,
+    ScrollPanelModule,
+    SharedPipesModule,
+    SidebarModule,
+    SliderModule,
+    SpinnerModule,
+    TableModule,
+    TabViewModule,
+    ToastModule,
+    ToolbarModule,
+    TooltipModule
+  ]
 })
 export class PrimeNgModulesModule {}
